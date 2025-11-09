552edd424370ad77abf27a51d56f4d46ee20b1db
ZhiYingSun <sophyzy.sun@mail.utoronto.ca>
2025-11-09 11:59:20 -0500

    remove the batch pipeline with this new pattern

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,14 +1,10 @@
 """
 Pipeline components for the Puerto Rico Restaurant Matcher
 """
-from .matching_pipeline import MatchingPipeline
-from .validation_pipeline import ValidationPipeline
 from .transformation_pipeline import TransformationPipeline
 from .orchestrator import PipelineOrchestrator
 
 __all__ = [
-    'MatchingPipeline',
-    'ValidationPipeline',
     'TransformationPipeline',
     'PipelineOrchestrator'
 ]
